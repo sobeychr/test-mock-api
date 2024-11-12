cd6bf0eeb3fd106d59d27d36c5c5e01794c64d93
Christopher Sobey <christopher.sobey@mindgeek.com>
2024-11-12 15:31:10 -0500

    +Added Logger +Added error handler +Added dev routes +Added routes dir

--- php/src/index.php
+++ php/src/index.php
@@ -1,4 +1,5 @@
 <?php
+
 error_reporting(-1);
 
 define('ROOT', dirname(__FILE__) . '/');
@@ -10,5 +11,26 @@ spl_autoload_register(function($classname) {
 $app = new App($_SERVER);
 
 require_once(ROOT . 'getFile.php');
-require_once(ROOT . 'middleware.php');
 require_once(ROOT . 'routes.php');
+
+if(file_exists(ROOT . 'routes.dev.php')) {
+    require_once(ROOT . 'routes.dev.php');
+}
+
+$routesDir = ROOT . 'routes/';
+if(file_exists($routesDir)) {
+    if($handle = opendir($routesDir)) {
+        while(false !== ($entry = readdir($handle))) {
+            if(str_contains($entry, '.php')) {
+                require_once($routesDir . $entry);
+            }
+        }
+    }
+}
+
+$app->error(function() {
+    return new Response([
+        'status' => 404,
+        'body' => 'Request not found',
+    ]);
+});
